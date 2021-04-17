3002750c226b82c4d33a0c56382b8162b444900c
Regae Laroya <rhlaroya@gmail.com>
2021-04-17 14:47:24 -0500

    Refactored a type in the repository

--- RedditClone/src/main/java/com/clone/reddit/repository/SubredditRepository.java
+++ RedditClone/src/main/java/com/clone/reddit/repository/SubredditRepository.java
@@ -3,7 +3,9 @@ package com.clone.reddit.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.clone.reddit.model.Subreddit;
+
 @Repository
-public interface SubredditRepository extends JpaRepository<SubredditRepository, Long> {
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
 
 }
