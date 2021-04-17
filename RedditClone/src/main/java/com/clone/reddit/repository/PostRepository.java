c02b35e84ffeccb2976c4c93b9ea8351fcc8beb0
Regae Laroya <rhlaroya@gmail.com>
2021-04-16 19:19:36 -0500

    Post Repository

---
+++ RedditClone/src/main/java/com/clone/reddit/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.clone.reddit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.clone.reddit.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
