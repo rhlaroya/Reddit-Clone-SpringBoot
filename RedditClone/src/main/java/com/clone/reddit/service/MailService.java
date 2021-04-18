1a62348eebcb5a9dd702a255cd65b739013a5088
Regae Laroya <rhlaroya@gmail.com>
2021-04-18 12:54:23 -0500

    Finished writing logic for mail service

---
+++ RedditClone/src/main/java/com/clone/reddit/service/MailService.java
@@ -0,0 +1,39 @@
+package com.clone.reddit.service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.clone.reddit.exception.SpringRedditException;
+import com.clone.reddit.model.NotificationEmail;
+
+import jdk.internal.org.jline.utils.Log;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+	
+	private final JavaMailSender mailSender;
+	private final MailContentBuilder mailContentBuilder;
+
+	void sendMail(NotificationEmail notificationEmail) {
+		MimeMessagePreparator messagePreparator = mimeMessage -> {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setFrom("springreddit@email.com");
+			messageHelper.setTo(notificationEmail.getRecipient());
+			messageHelper.setSubject(notificationEmail.getSubject());
+			messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+		};
+		try {
+			mailSender.send(messagePreparator);
+			log.info("Activation email sent!");
+		} catch(MailException e) {
+			throw new SpringRedditException("Exception occured when sending mail to" + notificationEmail.getRecipient());
+		}
+	}
+}
